97cb92c6ef84b73e554cbc0340dfb1d261ba7f3a
Andrey <bratand89@gmail.com>
2021-01-18 12:39:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+// let result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(typeof(answer));
+
+console.log(2*4 == "7");
