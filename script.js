6b97ed17231b9aeaa90d661989cec3cbce64f6bb
Andrey <bratand89@gmail.com>
2021-01-18 17:00:00 +0300

    first script

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-"use strict";
-// let result = confirm("Are you here");
-// console.log(result);
+'use strict';
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его?","");
 
-// const answer = prompt("Вам есть 18?", "");
-// console.log(typeof(answer));
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(2*4 == "7");
+console.log(personalMovieDB);
